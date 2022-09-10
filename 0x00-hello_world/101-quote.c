3b5e98230a99dd0602688a7407d6b9fa24d5432d
Josepheze <ezechukwujoseph2017@gmail.com>
2022-09-10 02:39:14 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *          followed by a new line, to standard error.
  *
- * Return: always 1 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
-		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"59);
+
+	return (1);
 }
